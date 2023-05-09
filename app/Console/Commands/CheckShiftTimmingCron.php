1d7c638a1f30bbd9ee0783a324efb5b37b031b26
Aqitech <102aqibshahzad@gmail.com>
2023-05-09 16:28:29 +0500

    first commit

--- app/Console/Commands/CheckShiftTimmingCron.php
+++ app/Console/Commands/CheckShiftTimmingCron.php
@@ -3,7 +3,9 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Services\ShiftManager;
+use App\Models\TimeSheet;
+use App\Models\User;
+use Carbon\Carbon;
 
 class CheckShiftTimmingCron extends Command
 {
@@ -35,8 +37,37 @@ class CheckShiftTimmingCron extends Command
      * Execute the console command.
      */
     public function handle() {
-        $entries_created = ShiftManager::createEntriesForCurrentShift();
+        $current_time = Carbon::now();
+        $current_time->tz('Asia/Karachi');
 
-        $this->info($entries_created . ' entries created successfully.');    
+        // Get the current time in AM/PM format
+        $current_pak_time = $current_time->format('h:i A');
+        $current_pak_date = $current_time->format('Y-m-d');
+        $current_pak_day = $current_time->format('l');
+
+        // Get the users who are currently on shift
+        $users_on_shift = User::where('start_time', '=', $current_pak_time)->where('is_deleted', 'N')->where('status', 'A')->get();
+
+        // Create a new entry in the timesheet table for each user on shift
+        foreach ($users_on_shift as $user) {
+            $createdSheets = Timesheet::where('user_id', $user->id)->where('date', $current_pak_date)->where('day', $current_pak_day)->orderBy('id', 'desc')->first();
+            if (!$createdSheets) {  
+                $entry = new Timesheet;
+                $entry->user_id = $user->id;
+                $entry->date = $current_pak_date;
+                $entry->day = $current_pak_day;
+                $entry->save();
+            } else {
+                return response()->json([
+                    'success' => true,
+                    'message' => count($users_on_shift) . ' entries already created.'
+                ]);
+            }
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => count($users_on_shift) . ' entries created successfully.'
+        ]);  
     }
 }
